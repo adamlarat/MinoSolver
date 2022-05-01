d132fc7b290bbfdd1e69fce41e19e248cc6883e4
Adam Larat <adam@larat.fr>
2022-05-01 10:14:12 +0200

    Generates all n-minos and find all covers on (pxm) grids... tested with pentaminos on (3x20) and (6x10) grids

---
+++ dlx.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  1 06:52:30 2022
+
+@author: larat
+"""
+
+### From https://www.cs.mcgill.ca/~aassaf9/python/algorithm_x.html
+
+def solve(X, Y, solution=[]):
+    if not X:
+        yield list(solution)
+    else:
+        c = min(X, key=lambda c: len(X[c]))
+        for r in list(X[c]):
+            solution.append(r)
+            cols = select(X, Y, r)
+            for s in solve(X, Y, solution):
+                yield s
+            deselect(X, Y, r, cols)
+            solution.pop()
+
+def select(X, Y, r):
+    cols = []
+    for j in Y[r]:
+        for i in X[j]:
+            for k in Y[i]:
+                if k != j:
+                    X[k].remove(i)
+        cols.append(X.pop(j))
+    return cols
+
+def deselect(X, Y, r, cols):
+    for j in reversed(Y[r]):
+        X[j] = cols.pop()
+        for i in X[j]:
+            for k in Y[i]:
+                if k != j:
+                    X[k].add(i)
