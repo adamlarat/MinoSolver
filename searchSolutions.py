ef192359c7b3795b714c3358602e74380a09d123
Adam Larat <adam@larat.fr>
2022-05-02 12:01:56 +0200

    Légères améliorations

--- searchSolutions.py
+++ searchSolutions.py
@@ -5,6 +5,34 @@ Created on Sun May  1 14:56:03 2022
 
 @author: larat
 """
+import numpy as np
+import pylab as plt
+from matplotlib import colors
+cmap = plt.cm.rainbow
+norm = colors.BoundaryNorm(np.arange(0.5, 12, 1), cmap.N)
+plt.close('all')
+
 sol1 = 'LLLLtNNNPPPLWWtttFNNPPUUWWVFFFl<<UnnWVFlllY<UUnnVVVYYYY'
 sol2 = 'NYYYYFlll**NNtYFFl****VNttnFF****VNt<nnPP***VVV<<nPPP**'
 sol3 = 'llnNYYYYUUUlnnNLLY*U*UlnWNNL*****PPWWNL<****PPPWWL<<***'
+
+grid = (5,11)
+solutions = [
+    '2666aa7755526caa77b54526cc17bbb442291118334499918888333',
+    '2666aa7755526caa77b53526cc17bbb332291118444399918888443',
+    '2666aa7733326caa77334426cc17bbb442291118b54599918888555',
+    '2666aa7744326caa77444326cc17bbb332291118b53599918888555',
+    '2666aa7744326caa77b44326cc17bb4332291118b53599918888555',
+    '2666aa7734426caa77b34426cc17bb3342291118b53599918888555',
+    '2666aa7755526caa77b53526cc17bb4332291118b44399918888443',
+    '2666aa7755526caa77b53526cc17bb3342291118b34499918888344',
+    '2666aa7755526caa77b54526cc17bbb442291113384499913338888',
+    '2666b88884426cbbb1844426cc771115522977aa13359997aa33355'
+    ]
+
+for sol in solutions:
+    mat = np.array([int(l,16) for l in sol]).reshape(grid)
+    plt.figure()
+    plt.pcolor(mat.reshape(grid),edgecolor='k',linewidth=2,cmap=cmap,norm=norm)
+    plt.show()
+
--- solveIQPuzzler2D.py
+++ solveIQPuzzler2D.py
@@ -9,7 +9,8 @@ import minos, dlx
 import numpy as np
 from solvePentaminos import findAllPositions,encode
 
-minos5 = {
+nminos = {
+    ### Pentaminos
     'F' : [1,6,7,8,12],
     'I' : [0,1,2,3, 4],
     'L' : [0,1,2,3, 6],
@@ -21,76 +22,71 @@ minos5 = {
     'W' : [1,2,6,7,12],
     'X' : [1,6,7,8,13],
     'Y' : [0,1,2,3, 7],
-    'Z' : [2,6,7,8,12]
- }
-
-minos4 = {
+    'Z' : [2,6,7,8,12],
+    ### Tetraminos
     'i' : [0,1,2,3],
     'l' : [0,1,2,5],
     'n' : [1,2,5,6],
     'o' : [0,1,5,6],
-    't' : [0,1,2,6]
-}
-
-minos3 = {
+    't' : [0,1,2,6],
+    ### Triminos
     '<' : [0,1,4],
-    '|' : [0,1,2]
+    '|' : [0,1,2],
+    ### Domino
+    '-' : [0,1],
+    ### Unimino
+    '.' : [0]
 }
 
-minos2 = {'-':[0,1]}
-
-minos1 = {'.':[0]}
-
-mySet = {}
-mySet['human'] = [
-    minos5['F'],
-    minos5['L'],
-    minos5['N'],
-    minos5['P'],
-    minos5['U'],
-    minos5['V'],
-    minos5['W'],
-    minos5['Y'],
-    minos4['l'],
-    minos4['n'],
-    minos4['t'],
-    minos3['<']
- ]
-mySet['typeToLetter'] = 'FLNPUVWYlnt<'
-mySet['letterToType'] = {letter : i for i,letter in enumerate(mySet['typeToLetter'])}
-mySet['base'] = []
-mySet['orientations'] = []
-mySet['types'] = []
-
-setSize = 0
-counter = 0
-for mino in mySet['human']: 
-    setSize += len(mino)
-    coords  = minos.toCoordinates(np.array(mino)[...,np.newaxis])
-    orientations = minos.getAllOrientations(coords)
-    mySet['base'].append(orientations[0])
-    mySet['orientations'] += orientations
-    mySet['types'] += [counter for i in range(len(orientations))]
-    counter += 1
-print(setSize)
-grid = (5,11)
-nCells = grid[0]*grid[1]
-assert nCells == setSize
-X,Y = findAllPositions(mySet,grid)
- 
-counter = 0
-file = open('SolutionsIQPuzzler2D_Grid%02ix%02i.txt'%(grid[0],grid[1]),'w')
-for solution in dlx.solve(X,Y):
-    counter += 1
-    if counter % 1000 == 0:
-        print("%06i solution"%counter)    
-    sol_mat = np.zeros(nCells,dtype=int)
-    for position in solution:
-        piece = Y[position]
-        sol_mat[piece[:-1]] = piece[-1]-nCells+1
-    file.write(encode(sol_mat)+'\n')
-    # plt.figure()
-    # plt.pcolor(sol_mat.reshape(grid),edgecolor='k',linewidth=2,cmap=cmap,norm=norm)
-    # plt.show()
-file.close()
+def generateSetup(listOfMinos):
+    mySet = {}
+    mySet['minosList'] = listOfMinos
+    mySet['human'] = [nminos[letter] for letter in listOfMinos]
+    mySet['minosType'] = {letter : i for i,letter in enumerate(mySet['minosList'])}
+    mySet['base'] = []
+    mySet['orientations'] = []
+    mySet['types'] = []
+    
+    setSize = 0
+    counter = 0
+    for mino in mySet['human']: 
+        setSize += len(mino)
+        coords  = minos.toCoordinates(np.array(mino)[...,np.newaxis])
+        orientations = minos.getAllOrientations(coords)
+        mySet['base'].append(orientations[0])
+        mySet['orientations'] += orientations
+        mySet['types'] += [counter for i in range(len(orientations))]
+        counter += 1
+    mySet['size'] = setSize
+    return mySet
+    
+    
+if __name__ == '__main__':
+    IQSolverMinos = 'FLNPUVWYlnt<'
+    pentaminos    = 'FLNPUVWYZTIX'
+    # mySet = generateSetup(pentaminos)
+    # grid = (3,20)
+    mySet = generateSetup(IQSolverMinos)
+    grid = (5,11)
+    print("Taille du set : ",mySet['size'])
+    nCells = grid[0]*grid[1]
+    assert nCells == mySet['size']
+    X,Y = findAllPositions(mySet,grid)
+     
+    # counter = 0
+    # file = open('Solutions_'+mySet['minosList']+'_Grid%02ix%02i.txt'%(grid[0],grid[1]),'w')
+    # for solution in dlx.solve(X,Y):
+    #     counter += 1
+    #     if counter % 1000 == 0:
+    #         print("%06i solution"%counter)    
+    #     sol_mat = np.zeros(nCells,dtype=int)
+    #     for position in solution:
+    #         piece = Y[position]
+    #         sol_mat[piece[:-1]] = piece[-1]-nCells+1
+    #     file.write(encode(sol_mat)+'\n')
+    #     # plt.figure()
+    #     # plt.pcolor(sol_mat.reshape(grid),edgecolor='k',linewidth=2,cmap=cmap,norm=norm)
+    #     # plt.show()
+    # file.close()
+    # print("Found %i solutions !"%counter)
 
